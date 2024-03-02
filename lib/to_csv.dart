b14527b61b2098e6b23913a8df6f9ffe5fbea934
Omar Yehia <omaryahiatawfiek@gmail.com>
2024-03-03 01:11:22 +0200

    quick hot fixes

--- lib/to_csv.dart
+++ lib/to_csv.dart
@@ -1,27 +1,31 @@
 library to_csv;
 
+import 'dart:io';
 import 'dart:convert';
 
-import 'package:file_saver/file_saver.dart';
-import 'package:flutter/foundation.dart';
 import 'package:csv/csv.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/foundation.dart';
+import 'package:file_saver/file_saver.dart';
 import 'package:share_plus/share_plus.dart';
-import 'dart:io';
 import 'package:universal_html/html.dart' as html;
 
- 
-Future myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,
-    {bool sharing = false, String? fileName, String? fileTimeStamp}) async {
- 
-  print("***** Gonna Create cv");
+Future myCSV(
+  List<String> headerRow,
+  List<List<String>> listOfListOfStrings, {
+  bool sharing = false,
+  String? fileName,
+  String? fileTimeStamp,
+}) async {
+  if (kDebugMode) {
+    print("***** Gonna Create cv");
+  }
   String givenFileName = fileName ?? "item_export";
 
   DateTime now = DateTime.now();
- 
-  String formattedDate =
-      fileTimeStamp ?? DateFormat('MM-dd-yyyy-HH-mm-ss').format(now);
- 
+
+  String formattedDate = fileTimeStamp ?? DateFormat('MM-dd-yyyy-HH-mm-ss').format(now);
+
   List<List<String>> headerAndDataList = [];
   headerAndDataList.add(headerRow);
   for (var dataRow in listOfListOfStrings) {
@@ -30,34 +34,33 @@ Future myCSV(List<String> headerRow, List<List<String>> listOfListOfStrings,
 
   String csvData = const ListToCsvConverter().convert(headerAndDataList);
 
- 
   if (kIsWeb) {
     final bytes = utf8.encode(csvData);
     final blob = html.Blob([bytes]);
     final url = html.Url.createObjectUrlFromBlob(blob);
     final anchor = html.document.createElement('a') as html.AnchorElement
       ..href = url
- 
       ..download = '$givenFileName$formattedDate.csv';
     html.document.body!.children.add(anchor);
     anchor.click();
     html.Url.revokeObjectUrl(url);
-  } else if (Platform.isAndroid ||
-      Platform.isIOS ||
-      Platform.isWindows ||
-      Platform.isMacOS) { 
+  } else if (Platform.isAndroid || Platform.isIOS || Platform.isWindows || Platform.isMacOS) {
     final bytes = utf8.encode(csvData);
     Uint8List bytes2 = Uint8List.fromList(bytes);
-    MimeType type = MimeType.csv;
+    MimeType mimeType = MimeType.csv;
     if (sharing == true) {
-      XFile xFile = XFile.fromData(bytes2, mimeType: type, name: givenFileName);
-      await Share.shareXFiles([xFile], text: 'Csv File');
+      XFile xFile = XFile.fromData(bytes2, mimeType: mimeType.type, name: givenFileName);
+      await Share.shareXFiles([xFile], text: 'CSV File');
     } else {
-      String formattedData = DateTime.now().millisecondsSinceEpoch.toString();
-      String? unknownValue = await FileSaver.instance
-          .saveAs(name: '$givenFileName_$formattedDate.csv', bytes: bytes2, ext: 'csv', mimeType: type);
-      print("Unknown value $unknownValue");
- 
+      String? unknownValue = await FileSaver.instance.saveAs(
+        name: '${givenFileName}_$formattedDate.csv',
+        bytes: bytes2,
+        ext: 'csv',
+        mimeType: mimeType,
+      );
+      if (kDebugMode) {
+        print("Unknown value $unknownValue");
+      }
     }
   }
 }
